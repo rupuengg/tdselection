0a073df9b306b909a57c7c5452ec2f187298e2e0
rupendrakumardhiman <rupendrakumardhiman@QAIT.COM>
2017-03-30 17:37:18 +0530

    Starting

---
+++ app.js
@@ -0,0 +1,79 @@
+;(function(ng){
+	ng
+	.module('hrisTemplating', [])
+	.controller('mainController', [
+		'$scope',
+		function($scope){
+			$scope.main = {
+				data : [
+					{
+						name : 'Test1',
+						email : 'test1gmail.com',
+						phone : '98989898'
+					},{
+						name : 'Test2',
+						email : 'test2gmail.com',
+						phone : '98989898'
+					},{
+						name : 'Test3',
+						email : 'test3gmail.com',
+						phone : '98989898'
+					},{
+						name : 'Test4',
+						email : 'test4gmail.com',
+						phone : '98989898'
+					}
+				],
+				startIndex : 0
+			};
+			
+			console.log('Main Controller', $scope.main);
+		}
+	])
+	.directive('timeSheetPanel', [
+		function(){
+			return {
+				restrict : 'E',
+				templateUrl : 'time-sheet-panel.html',
+				scope : true,
+				controller : [
+					'$scope',
+					function($scope){
+						$scope.mDown = function(attr1, attrs){
+							console.log(attr1, attrs);
+						};
+						
+						$scope.mUp = function(attr1, attrs){
+							console.log(attr1, attrs);
+						};
+					}
+				]
+			};
+		}
+	])
+	.directive('cellData', [
+		function(){
+			return {
+				restrict : 'E',
+				templateUrl : 'cell-data.html',
+				scope : {
+					it : '=',
+					current : '=',
+					mDown : '&',
+					mUp : '&'
+				},
+				link : function(scope, element, attrs){
+					element.on('mousedown', function(event){
+						console.log(event);
+						scope.mDown();
+					});
+					
+					element.on('mouseup', function(event){
+						console.log(event);
+						scope.mUp();
+					});
+				}
+			}
+		}
+	]);
+})(window.angular);
